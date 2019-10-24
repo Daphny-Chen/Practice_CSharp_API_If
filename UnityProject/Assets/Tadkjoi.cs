2bb2f7acf9089b16f41bb5b28681305160a7db9f
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-10-24 09:37:47 +0800

    1024

---
+++ UnityProject/Assets/Tadkjoi.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Tadkjoi : MonoBehaviour
+{
+
+    private Animator anr_eat;
+    private bool run;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anr_eat = gameObject.GetComponent<Animator>();
+        run = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anr_eat.SetTrigger("吃飯觸發器");
+
+        }
+        if ((Input.GetKey(KeyCode.R)))
+
+        {
+            anr_eat.SetBool("跑步開關", true);
+        }
+        else
+        {
+            anr_eat.SetBool("跑步開關", false);
+        }
+
+    }
+}
